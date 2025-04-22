7eeb1597fc119ade42c2b43ec93b58b8de4a4798
Ng462001 <nikhil462001@gmail.com>
2025-04-22 12:49:09 +0530

    update

--- BackEnd/employee/src/main/java/arc/employee/employee/EmployeeController.java
+++ BackEnd/employee/src/main/java/arc/employee/employee/EmployeeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = ${frontend.url})
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
